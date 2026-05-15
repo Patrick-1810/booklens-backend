4e29cb06f8b9c16cb8a8023f1f05fdc877fe52dc
Patrick-1810 <patrickprestes977@gmail.com>
2026-05-15 15:51:08 -0300

    git commit -m "feat: create route api"

--- app/main.py
+++ app/main.py
@@ -1,20 +1,10 @@
-from fastapi import FastAPI, UploadFile, File
-import pytesseract
-import cv2
-import numpy as np
+from fastapi import FastAPI
+from app.routes import ocr
 
-app = FastAPI()
+app = FastAPI(title="BookLens API")
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+app.include_router(ocr.router)
 
-@app.post("/extrair-texto")
-async def extrair_texto(file: UploadFile = File(...)):
-    contents = await file.read()
-    nparr = np.frombuffer(contents, np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    texto = pytesseract.image_to_string(gray, lang='por')
-
-    return {"texto_extraido": texto}
+@app.get("/")
+async def root():
+    return {"message": "BookLens API está funcionando!"}
