4cfa226b471a5bbc5d2af475a65ce7fb24d94fe7
Patrick-1810 <patrickprestes977@gmail.com>
2026-05-15 16:47:10 -0300

    git commit -m "feat: application of noise reduction, binarization, and perspective filters to the image "

--- app/core/processor.py
+++ app/core/processor.py
@@ -1,13 +1,45 @@
 import cv2
 import pytesseract
+import numpy as np
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 img = cv2.imread('../images_test/imagemBoa.jpeg')
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+if img is None:
+    print("Imagem não encontrada!")
+else:
 
-texto = pytesseract.image_to_string(gray, lang='por')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    denoised = cv2.fastNlMeansDenoising(gray, h=10)
+
+    binary = cv2.adaptiveThreshold(
+        denoised, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv2.THRESH_BINARY, 11, 2
+    )
+
+    coords = np.column_stack(np.where(binary > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+
+    (h, w) = binary.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(binary, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+
+    config = r'--psm 3'
+
+    try:
+        texto = pytesseract.image_to_string(rotated, lang='por', config=config)
+    except:
+        print("Erro ao carregar em 'por', tentando 'eng'...")
+        texto = pytesseract.image_to_string(rotated, lang='eng', config=config)
 
 print("--- Texto Extraído ---")
 print(texto)
+
+cv2.imwrite('resultado_pdi_img_boa.png', rotated)
