19c9ec855df7ed3ed70528e383d98381a0513421
Patrick-1810 <patrickprestes977@gmail.com>
2026-07-27 10:47:33 -0300

    feat(api): register dictionary autocomplete router in main application

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.database import models
 from app.database.config import engine
-from app.routes import ocr, auth
+from app.routes import ocr, auth, dictionary
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -18,6 +18,7 @@ app.add_middleware(
 
 app.include_router(ocr.router)
 app.include_router(auth.router)
+app.include_router(dictionary.router)
 
 @app.get("/")
 async def root():
